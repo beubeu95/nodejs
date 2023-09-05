057346f503a524a272bbf624605a5398b06c356d
beubeu95 <xuanjiong810@gmail.com>
2023-09-05 19:06:48 +0900

    nodejs start

---
+++ js01.js
@@ -0,0 +1,7 @@
+var a = 20;   
+let b = 20;
+const c = 20;
+//백틱(backtic = ``)으로 감싼 문장 => 템플릿(template)
+console.log("hello~! node js~!");
+console.log("var a :" +a+", let b :"+b+", const c :"+c);
+console.log(`var a : ${a}, let b : ${b}, const c : ${c}`);
