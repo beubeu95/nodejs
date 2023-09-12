9a28dda0f730a4bdceaa4fa2149a1f830b1f41fc
beubeu95 <xuanjiong810@gmail.com>
2023-09-12 20:16:20 +0900

    node js

--- js06.js
+++ js06.js
@@ -1,15 +1,15 @@
 //비구조 할당(Destructuring Assignment & Allocation) 
-// 배열과 같은 자료구조(reference type)를 단일 기억장소(primitive type)로 변경
-const person = ['kim', 'lee','hwang','park','oh'];
-let p1=person[0];
-let p2=person[1];
-let p3=person[2];
+//배열과 같은 자료구조(reference type)를 단일 기억장소(primitive type)로 변경
+const person = ['kim', 'lee', 'park', 'choi', 'oh']; //배열 구조
+let p1 = person[0];
+let p2 = person[1];
+let p3 = person[2];
 
-const member={
-    id : "kimhk",
-    age : 29,
-    addr : "가산동",
-    tel : "010-1111-2222"
+const member = {
+    id:"kkt",
+    age:42,
+    addr:"가산동",
+    tel:"02-1254-8282"
 }
 
 let id = member.id;
@@ -18,22 +18,24 @@ let addr = member.addr;
 let tel = member.tel;
 
 //배열 구조를 분해하여 비구조 할당
-let [k1,k2,k3,k4,k5] = person;
-[k2,k1,k4,k3,k5] = [k1,k2,k3,k4,k5]; // 비구조 할당의 값 교체
+let [k1, k2, k3, k4, k5] = person;
+[k2, k1, k4, k3, k5] = [k1, k2, k3, k4, k5];    //비구조 할당의 값 교체
 
-//값 정의X
-const [a,b,c] =["JAVA","C#"];
-console.log(`a: ${a}, b:${b}, c:${c}`); //c:undefined
+//UNDEFINED(비정의)
+const [a, b, c] = ["JAVA", "C#"];
+console.log(`a : ${a}, b : ${b}, c : ${c}`);    //c : undefined
 
 //초기값 할당
-const [d,e,f = 'NODE'] =["JAVA","C#"]; //f:NODE
-console.log(`d:${d}, e:${e}, f:${f}`);
+const [d, e, f = 'NODE'] = ["JAVA", "C#"]; // f : NODE
+console.log(`d : ${d}, e : ${e}, f : ${f}`);
 
 //일부값 무시
-const [g,,h,i]=['NODE','REACT','VUE','ANGULAR']; //i:ANGULAR
-console.log(`g:${g}, h:${h},i:${i}`);
+const [g, ,h, i] = ['NODE', 'REACT', 'VUE', 'ANGULAR']; 
+console.log(`g : ${g}, h : ${h}, i : ${i}`); //i : ANGULAR
 
-//객체의분해
-let {m1,m2,m3,m4} = member
-let {o1,o2,o3,o4} = {age:member.age, id:member.id};
+//객체의 분해
+//let {m1, m2, m3, m4} = member;
+let {o1, o2, o3, o4} = { age:member.age, id:member.id, addr:member.addr, tel:member.tel };
+
+
 
--- js14.js
+++ js14.js
@@ -1,25 +1,31 @@
 const fs = require("fs");
 let content = "나도 알아 ES6";
-fs.writeFile('./hk2.txt',content,(err) => {
-   fs.readFile('./hk2.txt','utf8',(err,data) => {
-      console.log(data);
-   });
+fs.writeFile('./kim2.txt', content, (err) => {
+    fs.readFile('./kim2.txt', 'utf8', (err, data) => {
+        console.log(data);
+    });
 });
-
-fs.writeFileSync('./hk3.txt',content);
-var data = fs.readFileSync('./hk3.txt', 'utf8');
+fs.writeFileSync('./kim3.txt', content);
+var data = fs.readFileSync('./kim3.txt', 'utf8');
 console.log(data);
 
 //내용 추가
-fs.readFile('./hk2.txt','utf8',(err,data) => {
-   let con = data + "내용추가";
-   fs.writeFile('./hk2.txt',con, (err) =>{
-      console.log(con);
-   });
+fs.readFile('./kim2.txt', 'utf8', (err, data) => {
+    let con = data+" 내용추가";
+    fs.writeFile('./kim2.txt', con, (err) => {
+        console.log(con);
+    });
 });
 
 /*
-fs.rename('./hk2.txt', './lee2.txt', (err) => {
-   
+fs.rename('./kim2.txt', './lee2.txt', (err) => {
+    console.log("파일이름 변경");
 });
+fs.renameSync('./kim2.txt', './lee2.txt');
+*/
+/*
+fs.unlink('./kim2.txt', (err) => {
+    console.log("파일 삭제")
+});
+fs.unlinkSync('./kim2.txt');
 */
